f548680f365bbd9892314af5b9c014c3d27cebb8
Andrew Gauger <andygauge@gmail.com>
2020-09-16 08:24:14 -0700

    30 days otp session

--- app/javascript/packs/timetable.js
+++ app/javascript/packs/timetable.js
@@ -1,1 +1,259 @@
-!function(e,t){"function"==typeof define&&define.amd?define(["exports"],t):t("undefined"!=typeof exports?exports:e.syncscroll={})}(this,function(e){var t="Width",n="Height",o="Top",r="Left",i="scroll",a="client",s=[],c=function(){var e,c,l,u,d,f=document.getElementsByClassName("syncscroll");for(d in s)if(s.hasOwnProperty(d))for(e=0;e<s[d].length;e++)s[d][e].removeEventListener("scroll",s[d][e].syn,0);for(e=0;e<f.length;e++)if(u=0,l=f[e],d=l.getAttribute("name")){for(l=l.scroller||l,s[d]||(s[d]=[]),c=0;c<s[d].length;c++)s[d][c]==l&&(u=1);u||s[d].push(l),l.eX=l.eY=0,function(e,c){e.addEventListener("scroll",e.syn=function(){var l,u,d=s[c],f=e[i+r],h=e[i+o],p=f/(e[i+t]-e[a+t]),m=h/(e[i+n]-e[a+n]),v=0,E=0;for(f!=e.eX&&(v=1,e.eX=f),h!=e.eY&&(E=1,e.eY=h),u=0;u<d.length;u++)l=d[u],l!=e&&(v&&(f=Math.round(p*(l[i+t]-l[a+t])),l.eX=l[i+r]=f),E&&(h=Math.round(m*(l[i+n]-l[a+n])),l.eY=l[i+o]=h))},0)}(l,d)}};"complete"==document.readyState?c():window.addEventListener("load",c,0),e.reset=c});var Timetable=function(){this.scope={hourStart:9,hourEnd:17},this.usingTwelveHour=!1,this.locations=[],this.events=[]};Timetable.Renderer=function(e){if(!(e instanceof Timetable))throw new Error("Initialize renderer using a Timetable");this.timetable=e},function(){function e(e,n){return t(e)&&t(n)}function t(e){return n(e)&&o(e)}function n(e){return e===parseInt(e,10)}function o(e){return e>=0&&e<24}function r(e,t){return t.indexOf(e)!==-1}function i(e,t){var n=e instanceof Date&&t instanceof Date,o=e<t;return n&&o}function a(e,t){return t>=e?t-e:24+t-e}function s(e){for(;e.firstChild;)e.removeChild(e.firstChild)}function c(e,t){var n;if(t){var o=e>=12?"PM":"AM";n=(e+11)%12+1+":00"+o}else{var r=e<10?"0":"";n=r+e+":00"}return n}Timetable.prototype={setScope:function(t,n){if(!e(t,n))throw new RangeError("Timetable scope should consist of (start, end) in whole hours from 0 to 23");return this.scope.hourStart=t,this.scope.hourEnd=n,this},useTwelveHour:function(){this.usingTwelveHour=!0},addLocations:function(e){function t(){return e instanceof Array}var n=this.locations;if(!t())throw new Error("Tried to add locations in wrong format");return e.forEach(function(e){if(r(e,n))throw new Error("Location already exists");n.push(e)}),this},addEvent:function(e,t,n,o,a){if(!r(t,this.locations))throw new Error("Unknown location");if(!i(n,o))throw new Error("Invalid time range: "+JSON.stringify([n,o]));var s="[object Object]"===Object.prototype.toString.call(a);return this.events.push({name:e,location:t,startDate:n,endDate:o,options:s?a:void 0}),this}},Timetable.Renderer.prototype={draw:function(e){function t(e){if(null===e)throw new Error("Timetable container not found")}function n(e){var t=e.appendChild(document.createElement("aside")),n=t.appendChild(document.createElement("ul"));o(n)}function o(e){for(var t=0;t<m.locations.length;t++){var n=e.appendChild(document.createElement("li")),o=n.appendChild(document.createElement("span"));o.className="row-heading",o.textContent=m.locations[t]}}function r(e){var t=e.appendChild(document.createElement("section")),n=i(t),o=t.appendChild(document.createElement("time"));o.className="syncscroll",o.setAttribute("name","scrollheader");var r=n.scrollWidth+"px";l(o,r)}function i(e){var t=e.appendChild(document.createElement("header"));t.className="syncscroll",t.setAttribute("name","scrollheader");for(var n=t.appendChild(document.createElement("ul")),o=!1,r=!1,i=m.scope.hourStart;!o;){var a=n.appendChild(document.createElement("li")),s=a.appendChild(document.createElement("span"));s.className="time-label",s.textContent=c(i,m.usingTwelveHour),i!==m.scope.hourEnd||m.scope.hourStart===m.scope.hourEnd&&!r||(o=!0),24===++i&&(i=0,r=!0)}return t}function l(e,t){var n=e.appendChild(document.createElement("ul"));n.style.width=t,n.className="room-timeline";for(var o=0;o<m.locations.length;o++){var r=n.appendChild(document.createElement("li"));u(m.locations[o],r)}}function u(e,t){for(var n=0;n<m.events.length;n++){var o=m.events[n];o.location===e&&d(o,t)}}function d(e,t){var n,o,r,i=void 0!==e.options,a=!1;i&&(n=void 0!==e.options.url,o=void 0!==e.options["class"],r=void 0!==e.options.data,a=void 0!==e.options.onClick);var s=n?"a":"span",c=t.appendChild(document.createElement(s)),l=c.appendChild(document.createElement("small"));if(c.title=e.name,n&&(c.href=e.options.url),r)for(var u in e.options.data)c.setAttribute("data-"+u,e.options.data[u]);a&&c.addEventListener("click",function(t){e.options.onClick(e,m,t)}),c.className=o?"time-entry "+e.options["class"]:"time-entry",c.style.width=f(e),c.style.left=p(e),l.textContent=e.name}function f(e){var t=e.startDate,n=e.endDate,o=h(t,n);return o/v*100+"%"}function h(e,t){return(t.getTime()-e.getTime())/1e3/60/60}function p(e){var t=m.scope.hourStart,n=e.startDate.getHours()+e.startDate.getMinutes()/60,o=a(t,n);return o/v*100+"%"}var m=this.timetable,v=a(m.scope.hourStart,m.scope.hourEnd),E=document.querySelector(e);t(E),s(E),n(E),r(E),syncscroll.reset()}}}();
+/*jshint -W079*/
+
+'use strict';
+
+var Timetable = function() {
+    this.scope = {
+        hourStart: 9,
+        hourEnd: 17
+    };
+    this.usingTwelveHour = false;
+    this.locations = [];
+    this.events = [];
+};
+
+Timetable.Renderer = function(tt) {
+    if (!(tt instanceof Timetable)) {
+        throw new Error('Initialize renderer using a Timetable');
+    }
+    this.timetable = tt;
+};
+
+(function() {
+    function isValidHourRange(start, end) {
+        return isValidHour(start) && isValidHour(end);
+    }
+    function isValidHour(number) {
+        return isInt(number) && isInHourRange(number);
+    }
+    function isInt(number) {
+        return number === parseInt(number, 10);
+    }
+    function isInHourRange(number) {
+        return number >= 0 && number < 24;
+    }
+    function locationExistsIn(loc, locs) {
+        return locs.indexOf(loc) !== -1;
+    }
+    function isValidTimeRange(start, end) {
+        var correctTypes = start instanceof Date && end instanceof Date;
+        var correctOrder = start < end;
+        return correctTypes && correctOrder;
+    }
+    function getDurationHours(startHour, endHour) {
+        return endHour >= startHour ? endHour - startHour : 24 + endHour - startHour;
+    }
+
+    Timetable.prototype = {
+        setScope: function(start, end) {
+            if (isValidHourRange(start, end)) {
+                this.scope.hourStart = start;
+                this.scope.hourEnd = end;
+            } else {
+                throw new RangeError('Timetable scope should consist of (start, end) in whole hours from 0 to 23');
+            }
+
+            return this;
+        },
+        useTwelveHour: function(){
+            this.usingTwelveHour = true;
+        },
+        addLocations: function(newLocations) {
+            function hasProperFormat() {
+                return newLocations instanceof Array;
+            }
+
+            var existingLocations = this.locations;
+
+            if (hasProperFormat()) {
+                newLocations.forEach(function(loc) {
+                    if (!locationExistsIn(loc, existingLocations)) {
+                        existingLocations.push(loc);
+                    } else {
+                        throw new Error('Location already exists');
+                    }
+                });
+            } else {
+                throw new Error('Tried to add locations in wrong format');
+            }
+
+            return this;
+        },
+        addEvent: function(name, location, start, end, tooltip, options) {
+            if (!locationExistsIn(location, this.locations)) {
+                throw new Error('Unknown location');
+            }
+            if (!isValidTimeRange(start, end)) {
+                throw new Error('Invalid time range: ' + JSON.stringify([start, end]));
+            }
+
+            var optionsHasValidType = Object.prototype.toString.call(options) === '[object Object]';
+
+            this.events.push({
+                name: name,
+                location: location,
+                startDate: start,
+                endDate: end,
+                tooltip: tooltip,
+                options: optionsHasValidType ? options : undefined
+            });
+
+            return this;
+        }
+    };
+
+    function emptyNode(node) {
+        while (node.firstChild) {
+            node.removeChild(node.firstChild);
+        }
+    }
+
+    function prettyFormatHour(hour, usingTwelveHour) {
+        var prettyHour;
+        if(usingTwelveHour) {
+            var period = hour >= 12 ? 'PM':'AM';
+            prettyHour = ((hour + 11) % 12 + 1) + ':00' + period;
+        } else {
+            var prefix = hour < 10 ? '0' : '';
+            prettyHour = prefix + hour + ':00';
+        }
+        return prettyHour;
+    }
+
+    Timetable.Renderer.prototype = {
+        draw: function(selector) {
+            var timetable = this.timetable;
+
+            function checkContainerPrecondition(container) {
+                if (container === null) {
+                    throw new Error('Timetable container not found');
+                }
+            }
+            function appendTimetableAside(container) {
+                var asideNode = container.appendChild(document.createElement('aside'));
+                var asideULNode = asideNode.appendChild(document.createElement('ul'));
+                appendRowHeaders(asideULNode);
+            }
+            function appendRowHeaders(ulNode) {
+                for (var k=0; k<timetable.locations.length; k++) {
+                    var liNode = ulNode.appendChild(document.createElement('li'));
+                    var spanNode = liNode.appendChild(document.createElement('span'));
+                    spanNode.className = 'row-heading';
+                    spanNode.textContent = timetable.locations[k];
+                }
+            }
+            function appendTimetableSection(container) {
+                var sectionNode = container.appendChild(document.createElement('section'));
+                var headerNode = appendColumnHeaders(sectionNode);
+                var timeNode = sectionNode.appendChild(document.createElement('time'));
+                timeNode.className = 'syncscroll';
+                timeNode.setAttribute('name', 'scrollheader');
+                var width = headerNode.scrollWidth + 'px';
+                appendTimeRows(timeNode, width);
+            }
+            function appendColumnHeaders(node) {
+                var headerNode = node.appendChild(document.createElement('header'));
+                headerNode.className = 'syncscroll';
+                headerNode.setAttribute('name', 'scrollheader');
+                var headerULNode = headerNode.appendChild(document.createElement('ul'));
+
+                var completed = false;
+                var looped = false;
+
+                for (var hour=timetable.scope.hourStart; !completed;) {
+                    var liNode = headerULNode.appendChild(document.createElement('li'));
+                    var spanNode = liNode.appendChild(document.createElement('span'));
+                    spanNode.className = 'time-label';
+                    spanNode.textContent = prettyFormatHour(hour, timetable.usingTwelveHour);
+
+                    if (hour === timetable.scope.hourEnd && (timetable.scope.hourStart !== timetable.scope.hourEnd || looped)) {
+                        completed = true;
+                    }
+                    if (++hour === 24) {
+                        hour = 0;
+                        looped = true;
+                    }
+                }
+                return headerNode;
+            }
+            function appendTimeRows(node, width) {
+                var ulNode = node.appendChild(document.createElement('ul'));
+                ulNode.style.width = width;
+                ulNode.className = 'room-timeline';
+                for (var k=0; k<timetable.locations.length; k++) {
+                    var liNode = ulNode.appendChild(document.createElement('li'));
+                    appendLocationEvents(timetable.locations[k], liNode);/**/
+                }
+            }
+            function appendLocationEvents(location, node) {
+                for (var k=0; k<timetable.events.length; k++) {
+                    var event = timetable.events[k];
+                    if (event.location === location) {
+                        appendEvent(event, node);
+                    }
+                }
+            }
+            function appendEvent(event, node) {
+                var hasOptions = event.options !== undefined;
+                var hasURL, hasAdditionalClass, hasDataAttributes, hasClickHandler = false;
+
+                if (hasOptions) {
+                    hasURL = event.options.url !== undefined;
+                    hasAdditionalClass = event.options.class !== undefined;
+                    hasDataAttributes = event.options.data !== undefined;
+                    hasClickHandler = event.options.onClick !== undefined;
+                }
+
+                var elementType = hasURL ? 'a' : 'span';
+                var eventNode = node.appendChild(document.createElement(elementType));
+                var smallNode = eventNode.appendChild(document.createElement('small'));
+                eventNode.title = event.name;
+
+                if (hasURL) {
+                    eventNode.href = event.options.url;
+                }
+
+                if (hasDataAttributes){
+                    for (var key in event.options.data) {
+                        eventNode.setAttribute('data-'+key, event.options.data[key]);
+                    }
+                }
+
+                if (hasClickHandler) {
+                    eventNode.addEventListener('click', function(e) {
+                        event.options.onClick(event, timetable, e);
+                    });
+                }
+
+                eventNode.className = hasAdditionalClass ? 'time-entry ' + event.options.class : 'time-entry';
+                eventNode.style.width = computeEventBlockWidth(event);
+                eventNode.style.left = computeEventBlockOffset(event);
+                smallNode.textContent = event.name;
+            }
+            function computeEventBlockWidth(event) {
+                var start = event.startDate;
+                var end = event.endDate;
+                var durationHours = computeDurationInHours(start, end);
+                return durationHours / scopeDurationHours * 100 + '%';
+            }
+            function computeDurationInHours(start, end) {
+                return (end.getTime() - start.getTime()) / 1000 / 60 / 60;
+            }
+            function computeEventBlockOffset(event) {
+                var scopeStartHours = timetable.scope.hourStart;
+                var eventStartHours = event.startDate.getHours() + (event.startDate.getMinutes() / 60);
+                var hoursBeforeEvent =  getDurationHours(scopeStartHours, eventStartHours);
+                return hoursBeforeEvent / scopeDurationHours * 100 + '%';
+            }
+
+            var scopeDurationHours = getDurationHours(timetable.scope.hourStart, timetable.scope.hourEnd);
+            var container = document.querySelector(selector);
+            checkContainerPrecondition(container);
+            emptyNode(container);
+            appendTimetableAside(container);
+            appendTimetableSection(container);
+            syncscroll.reset();
+        }
+    };
+
+})();
