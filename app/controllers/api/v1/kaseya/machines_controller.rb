015337597dfdea0c10574f7c1166c05582e570a4
Andrew Gauger <andygauge@gmail.com>
2021-06-29 15:41:11 -0700

    rename controller class

--- app/controllers/api/v1/kaseya/machines_controller.rb
+++ app/controllers/api/v1/kaseya/machines_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     module Kaseya
-      class AlertsController < ApiV1Controller
+      class MachinesController < ApiV1Controller
         include KaseyaApi
 
         def show
